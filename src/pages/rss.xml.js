4fd31e7158afcfd57d9a2e81d1c04bb38c1b4695
endigma <endigma@mailcat.ca>
2024-04-02 10:05:49 -0300

    filter xml feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -8,9 +8,11 @@ export async function GET(context) {
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
     site: context.site,
-    items: posts.map((post) => ({
-      ...post.data,
-      link: `/blog/${post.slug}/`,
-    })),
+    items: posts
+      .map((post) => ({
+        ...post.data,
+        link: `/blog/${post.slug}/`,
+      }))
+      .filter((p) => p.pubDate < new Date()),
   });
 }
